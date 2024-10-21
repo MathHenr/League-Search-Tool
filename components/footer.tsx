d253ab13fd6303f231b9fec5800b0f63d913b086
Matheus Henrique <matheusobrinho2k20@gmail.com>
2024-10-21 00:50:36 -0300

    Finishing the sign-in and sign-up pages

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <div className="absolute lg:hidden w-full h-16 bottom-0 bg-gradient-to-l from-[#FF0D09] via-[#FF4A31] to-[#FF704A]">
+            <div className="h-full flex items-center justify-center">
+                <span className="text-base font-normal text-white">Made with ❤ by Matheus</span>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,12 @@
-import { NextResponse } from "next/server";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/"]);
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
 
 export default clerkMiddleware((auth, request) => {
-    if (isProtectedRoute(request)) {
-        auth().protect()
-    }
-
-    return NextResponse.next()
-});
+  if (!isPublicRoute(request)) {
+    auth().protect()
+  }
+})
 
 export const config = {
   matcher: [
