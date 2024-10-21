d2bc36d466cfb36fe271f926a42d8d9818757597
Matheus Henrique <matheusobrinho2k20@gmail.com>
2024-10-21 16:44:51 -0300

    Creating homepage

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,9 @@
 const Footer = () => {
     return (
         <div className="absolute lg:hidden w-full h-16 bottom-0 bg-gradient-to-l from-[#FF0D09] via-[#FF4A31] to-[#FF704A]">
-            <div className="h-full flex items-center justify-center">
-                <span className="text-base font-normal text-white">Made with ❤ by Matheus</span>
+            <div className="w-full flex-col place-items-center justify-items-center">
+                <p>All Rights Reserved by Riot Games.</p>
+                <p className="text-base font-normal text-white">Made with ❤ by Matheus</p>
             </div>
         </div>
     )
