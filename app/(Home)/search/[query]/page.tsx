53a96eb3232e969f7d6fdd5a4bf5ff2f0679d394
Matheus Henrique <matheusobrinho2k20@gmail.com>
2024-10-23 21:25:58 -0300

    Start to work on default-screen branch

--- app/(Home)/search/[query]/page.tsx
+++ app/(Home)/search/[query]/page.tsx
@@ -23,6 +23,7 @@ export default function SearchQuery () {
                     <div className="w-full border-b border-slate-100" />
                 </div>
             )}
+            {/* initial commit of the branch default screen */}
             <DefaultScreen />
         </div>
     )
